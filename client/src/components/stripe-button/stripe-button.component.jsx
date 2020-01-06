641f53b2377c5c2a8bbb73c5822417521ec735b3
nixdie <nixdie@yandex.ru>
2020-01-06 13:07:13 +0700

    updated stripe-button, connected with backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_YNyizSISuCsMkl6Y5KWmkYAc00MBCD43al';
 
   const onToken = token => {
-    console.log(token)
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful ✔✔✔')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment 😱😱😱 / Please sure you use the provided credit card.')
+    })
   }
 
   return (
